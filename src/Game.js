9957f7d414c21604263b0087103d5205f5e9d8d3
Gabriel Trompiz <gab.tc@icloud.com>
2018-06-25 20:36:10 -0400

    bug fixing

--- src/Game.js
+++ src/Game.js
@@ -5,20 +5,29 @@ import Box from './Box';
 class Game extends Component {
   constructor(props) {
     super(props);
-    this.state = {reload: null};
+    this.state = {positions: this.randomLocations()};
   }
+  
   renderBoxes() {
-    let positions = [];
-    while(positions.length < 5){
-      var randomnumber = Math.floor(Math.random() * 16);
-      if(positions.indexOf(randomnumber) > -1) continue;
-      positions[positions.length] = randomnumber;
-    }
     var boxes = []
     for (var i = 0; i < 16; i++) {
-      boxes.push(<Box id={i} key={i} positions={positions}/>);
+      boxes.push(<Box id={i} key={i} positions={this.state.positions}/>);
     }
     return boxes;
+  }
+
+  randomLocations() {
+    var positionsArray = [12];
+    while(positionsArray.length < 5){
+      var randomnumber = Math.floor(Math.random() * 16);
+      if(positionsArray.indexOf(randomnumber) > -1) continue;
+      positionsArray[positionsArray.length] = randomnumber;
+    }
+    return positionsArray;
+  }
+
+  reload() {
+    this.setState({positions: this.randomLocations()});
   }
 
   render(){
@@ -30,10 +39,6 @@ class Game extends Component {
         <button type="button" onClick={() => this.reload()} className="button">Refresh</button>
       </div>
     );
-  }
-
-  reload() {
-    this.setState({relaod: true});
   }
 }
 
